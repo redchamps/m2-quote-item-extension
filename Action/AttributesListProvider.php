d0f44f60d6ee46ced61fb094b6a27dabd11ef02f
Ravinder <rav@redchamps.com>
2024-06-16 13:18:46 +0530

    changed extension attribute names in order to avoid conflict with default properties

--- Action/AttributesListProvider.php
+++ Action/AttributesListProvider.php
@@ -7,14 +7,14 @@ class AttributesListProvider
     public function get()
     {
         return [
-            'row_weight' => 'setRowWeight',
-            'tax_percent' => 'setTaxPercent',
-            'tax_amount' => 'setTaxAmount',
-            'price_incl_tax' => 'setPriceInclTax',
-            'row_total_incl_tax' => 'setRowTotalInclTax',
-            'discount_amount' => 'setDiscountAmount',
-            'row_total_with_discount' => 'setRowTotalWithDiscount',
-            'row_total' => 'setRowTotal',
+            'row_weight' => 'setItemRowWeight',
+            'tax_percent' => 'setItemTaxPercent',
+            'tax_amount' => 'setItemTaxAmount',
+            'price_incl_tax' => 'setItemPriceInclTax',
+            'row_total_incl_tax' => 'setItemRowTotalInclTax',
+            'discount_amount' => 'setItemDiscountAmount',
+            'row_total_with_discount' => 'setItemRowTotalWithDiscount',
+            'row_total' => 'setItemRowTotal',
         ];
     }
 }
